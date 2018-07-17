aa465a6c6b56c22797a60ba6e97837d7f83f0645
tim e <tim.essam@gmail.com>
2018-07-16 22:35:42 -0400

    outline strategy for reshaping

--- 00_Clean_VamosLer.R
+++ 00_Clean_VamosLer.R
@@ -17,9 +17,14 @@ df <- read_excel(file.path(datapath, ler_data),
                  sheet = "Lista Geral das Escolas")
 
 
+# Load and clean data -----------------------------------------------------
+
 # The first 4 rows are gibberish and need to be cleaned up, so let's extract the 5:length(df)
   df_data <- df[5:dim(df)[1], ]
 
 # Now need to fix up the first four rows
+# Step 1. Identify all columns that have totals -- drop them
+# Step 2. Create standardized names for each column that will facilitate a reshape
+# Step 3. 
             
             
